4431f0bf468c5e1d250540cc79a993d31ccffc7d
Unknoweds <alexandrefidutra@gmail.com>
2026-05-30 18:06:41 +0000

    Movimentação das peças TORRE, BISPO e RAINHA

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
+// Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
@@ -27,6 +24,32 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+#include <stdio.h>
+//MOVIMENTO DAS PEÇAS BRANCAS DE XADREZ
+int main() {
+//movimento da torre 5 casas para a direita
+    // Representar colunas como números (1 = coluna A, 6 = coluna F)
+    int torre_posicao = 1; // posição inicial da torre (A1 -> coluna 1)
+    for (torre_posicao = 1; torre_posicao <= 6; torre_posicao++) { // mover até coluna 6 (A->F)
+        printf("Direita: Torre na coluna %d\n", torre_posicao);
+    }
+//movimento do bispo 5 casas para a diagonal cima direita
+    int bispo_coluna = 3; // posição inicial do bispo (C3 -> coluna 3)
+    do {
+        printf("Direita: Bispo na coluna ( %d)\n", bispo_coluna);
+        bispo_coluna++; // mover para a coluna  (coluna +1)
+    } while (bispo_coluna <= 8); // mover até coluna 8 (C->H)
 
+    int bispo_linha = 1; // posição inicial do bispo (C1 -> linha 1)
+    do {
+        printf("Cima: Bispo na linha ( %d)\n", bispo_linha);
+        bispo_linha++; // mover para a linha (linha +1)
+    } while (bispo_linha <= 6); // mover até linha 6 (1->6)
+//movimento da rainha 8 casas para a esquerda
+    int rainha_coluna = 8; // posição inicial da rainha (H1 -> coluna 8)
+    while (rainha_coluna >= 1) { // mover até coluna 1 (H->A)
+        printf("Esquerda: Rainha na coluna %d\n", rainha_coluna);
+        rainha_coluna--; // mover para a coluna (coluna -1)
+    }
     return 0;
 }
